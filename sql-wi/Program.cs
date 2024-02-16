714485eb619401530877c3172637d4d75fc13148
anevjes <you@example.com>
2024-02-16 11:32:48 +0800

    workibg

--- sql-wi/Program.cs
+++ sql-wi/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using Microsoft.Data.SqlClient;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 
 class Program
 {
     static void Main()
     {
+
+        // try out aZKV:
+        var keyVaultName = "azwi-kv1312anevjes"; 
+        var keyVaultUrl = $"https://{keyVaultName}.vault.azure.net/";
+        
+        //Thread.Sleep(100000);
+
+
+        var secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+        var secret = secretClient.GetSecret("someSecret");
+
+        Console.WriteLine("Trying fetching Secret from keyvault using DefualtCredentials: " + secret.Value.Value);
+
+
         // Connection string with Active Directory Default authentication
-        var connectionString = "Server=anevjes-sql-server.database.windows.net; Authentication=Active Directory MSI; Encrypt=True; User Id=21129c0c-e72a-408b-b589-96b88f9a24c2; Database=anevjes-sql-001";
+        var connectionString = "Server=anevjes-sql-server.database.windows.net; Authentication=Active Directory Default; Encrypt=True; Database=anevjes-sql-001";
         Console.WriteLine("Connection string: " + connectionString);
 
         using (SqlConnection connection = new SqlConnection(connectionString))
